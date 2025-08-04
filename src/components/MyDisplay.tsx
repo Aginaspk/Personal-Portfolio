fe887cf1f9c566c0eed12cb82f12d49d978266c4
Aginas pk <aginaspk6@gmail.com>
2025-08-04 15:48:09 +0530

    spotify

--- src/components/MyDisplay.tsx
+++ src/components/MyDisplay.tsx
@@ -10,7 +10,26 @@ import { ArrowRight, Code } from "lucide-react";
 const MyDisplay = () => {
   return (
     <div className="mx-auto md:px-5 grid w-full auto-rows-auto md:auto-rows-[19rem] grid-cols-6 gap-4 md:max-w-full my-20">
-      <div className="col-span-6 md:col-span-3 lg:col-span-4 w-full min-h-[21rem] md:min-h-0 rounded-lg [border:1px_solid_rgba(255,255,255,.1)] [box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]"></div>
+      <div className="col-span-6 md:col-span-3 lg:col-span-4 w-full min-h-[21rem] md:min-h-0 rounded-lg [border:1px_solid_rgba(255,255,255,.1)] [box-shadow:0_-20px_80px_-20px_#ffffff1f_inset] relative overflow-hidden">
+        <div>
+          <Technology />
+        </div>
+        <div className="pointer-events-none z-30 flex transform-gpu flex-col gap-1 p-6 transition-all duration-300 group-hover:-translate-y-10 absolute bottom-0 w-full">
+          <Code
+            size={40}
+            className="text-neutral-700 transition-all duration-300 ease-in-out group-hover:scale-75 mb-1"
+          />
+          <h1 className="max-w-lg text-neutral-400 font-outfit">Tech Stack</h1>
+        </div>
+        <div className="absolute bottom-0 flex w-full translate-y-10 transform-gpu flex-row items-center p-6 opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100 z-50 font-outfit">
+          <h4 className="text-neutral-300 flex items-center gap-2 font-bold cursor-pointer">
+            Explore Stack{" "}
+            <span className="pt-[2px]">
+              <ArrowRight />
+            </span>
+          </h4>
+        </div>
+      </div>
 
       <div className="group col-span-6 md:col-span-3 lg:col-span-2 md:row-span-2 w-full min-h-[25rem] md:min-h-0 rounded-lg [border:1px_solid_rgba(255,255,255,.1)] [box-shadow:0_-20px_80px_-20px_#ffffff1f_inset] relative overflow-hidden z-50">
         <h1 className="mt-4 w-full bg-linear-to-b from-[#edeffd] to-[#7b9cda] bg-clip-text text-center text-xl md:text-2xl leading-[100%] font-bold tracking-tighter text-balance text-transparent select-none md:mt-12 absolute z-100 px-4">
@@ -67,24 +86,27 @@ const MyDisplay = () => {
       </div>
       {/* last */}
       <div className="group col-span-6 md:col-span-6 lg:col-span-4 w-full min-h-[21rem] md:min-h-0 rounded-lg [border:1px_solid_rgba(255,255,255,.1)] [box-shadow:0_-20px_80px_-20px_#ffffff1f_inset] relative overflow-hidden">
-        <div>
-          <Technology />
-        </div>
-        <div className="pointer-events-none z-30 flex transform-gpu flex-col gap-1 p-6 transition-all duration-300 group-hover:-translate-y-10 absolute bottom-0 w-full">
-          <Code
-            size={40}
-            className="text-neutral-700 transition-all duration-300 ease-in-out group-hover:scale-75 mb-1"
+        <div
+          style={{
+            left: 0,
+            width: "100%",
+            height: 352,
+            position: "relative",
+          }}
+        >
+          <iframe
+            src="https://open.spotify.com/embed/playlist/6JM0XehGZlPfrFHefkLFcJ?utm_source=generator&theme=0"
+            style={{
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              position: "absolute",
+              border: 0,
+            }}
+            allowFullScreen
+            allow="clipboard-write *; encrypted-media *; fullscreen *; picture-in-picture *;"
           />
-          <h1 className="max-w-lg text-neutral-400 font-outfit">Tech Stack</h1>
-          
-        </div>
-        <div className="absolute bottom-0 flex w-full translate-y-10 transform-gpu flex-row items-center p-6 opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100 z-50 font-outfit">
-          <h4 className="text-neutral-300 flex items-center gap-2 font-bold cursor-pointer">
-            Explore Stack{" "}
-            <span className="pt-[2px]">
-              <ArrowRight />
-            </span>
-          </h4>
         </div>
       </div>
     </div>
